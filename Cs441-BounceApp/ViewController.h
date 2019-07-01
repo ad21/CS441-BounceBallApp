5b154da6542d1ce66ae26959dd7912e67ffb54cf
Adwait Magikar <amagika1@binghamton.edu>
2019-07-01 19:18:51 -0400

    Initial Commit

---
+++ Cs441-BounceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cs441-BounceApp
+//
+//  Created by Adwait Magikar on 7/1/19.
+//  Copyright © 2019 Adwait Magikar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Cs441-BounceApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Cs441-BounceApp
+//
+//  Created by Adwait Magikar on 7/1/19.
+//  Copyright © 2019 Adwait Magikar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
