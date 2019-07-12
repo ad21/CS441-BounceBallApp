426bad1804139630f81cec6f7b96a072eadff264
Adwait Magikar <amagika1@binghamton.edu>
2019-07-11 22:01:24 -0400

    Updated other animations like bounce and collision to play around with the 3 labels,also linked the IBAction Button to the UI Button

---
+++ Cs441-BounceApp/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Cs441-BounceApp
+//
+//  Created by Adwait Magikar on 7/1/19.
+//  Copyright © 2019 Adwait Magikar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+{
+    IBOutlet UILabel *Football;
+    IBOutlet UILabel *Baseball;
+    IBOutlet UILabel *Tennisball;
+    
+}
+
+@end
---
+++ Cs441-BounceApp/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  Cs441-BounceApp
+//
+//  Created by Adwait Magikar on 7/1/19.
+//  Copyright © 2019 Adwait Magikar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    UIDynamicAnimator *animator;
+    UIGravityBehavior *gravity;
+    UICollisionBehavior *collision;
+    UIDynamicItemBehavior *bounce;
+}
+
+@end
+
+@implementation ViewController
+
+-(IBAction)buttonIsClicked:(id)sender
+{
+    animator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
+    
+    gravity = [[UIGravityBehavior alloc]initWithItems:@[Football,Tennisball,Baseball]];
+    
+    [animator addBehavior:gravity];
+    
+    collision = [[UICollisionBehavior alloc]initWithItems:@[Football,Tennisball,Baseball]];
+    
+    collision.translatesReferenceBoundsIntoBoundary = YES;
+    
+    [animator addBehavior:collision];
+    
+    bounce = [[UIDynamicItemBehavior alloc]initWithItems:@[Football,Tennisball,Baseball]];
+    
+    [animator addBehavior:bounce];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
